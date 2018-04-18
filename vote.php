b6f1b72cb4b83472001752162d697fb9218906b8
mahesh1301 <30906056+mahesh1301@users.noreply.github.com>
2018-04-18 13:38:39 +0530

    Create vote.php

---
+++ vote.php
@@ -0,0 +1,31 @@
+<?php include('config.php');
+	
+	session_start();
+	$sect=$_SESSION['sec'];
+	$_SESSION['id']=$sect;
+	if(isset($_POST['password']))
+	{
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+	}
+	
+	$sql = "select password from login where name = '$username'";
+	$result = mysqli_query($conn, $sql) or die ("Error");
+	
+	$row = mysqli_fetch_assoc($result);
+	
+	
+	
+	if($row['password']==$password)
+	{
+		$_SESSION['user'] = $username;
+		header('Location:votelist.php');
+	}
+	else
+	{
+		header('Location:index.php');
+	}
+
+	mysqli_close($conn);
+
+?>
